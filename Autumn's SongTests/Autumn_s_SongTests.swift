72a218bd0bdb18447de987b04d290432163b33ca
Eder Rengifo <ederrengifo@iMac-de-Eder.local>
2014-12-31 22:01:12 -0500

    Initial Commit

---
+++ Autumn's SongTests/Autumn_s_SongTests.swift
@@ -0,0 +1,36 @@
+//
+//  Autumn_s_SongTests.swift
+//  Autumn's SongTests
+//
+//  Created by Eder Rengifo on 31/12/14.
+//  Copyright (c) 2014 Eder Rengifo. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Autumn_s_SongTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
